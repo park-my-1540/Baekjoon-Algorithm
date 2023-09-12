b181a40f6065487e6d9862dfc0a15ff152dd295a
sia <sia.park@tosslab.com>
2023-09-12 15:30:53 +0900

    좌표정렬하기 수정

--- sort/01/index.js
+++ sort/01/index.js
@@ -5,28 +5,31 @@
  *      (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
  * 출력 : 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
  */
-
 const fs = require('fs');
-const inputData = fs.readFileSync('text.txt').toString().split('\n');
+const path = require('path'); // path 모듈 추가
+const filePath = path.join(__dirname, 'example.txt');
 
+const inputData = fs.readFileSync(filePath).toString().split('\n');
 const length = Number(inputData[0]);
 let array = [];
 
 for(let i=1; i<=length; i++){
-    const [x,y] = inputData[i].toString(' ').map(Number);
+    const [x,y] = inputData[i].split(' ').map(Number);
     array.push([x,y]);
 }
 // a는 x좌표 배열, b는 y좌표 배열
 const compare = (a,b) => {
-    if(a[0] !== b[0]){
-        return a[0].length - b[0].length;
-    }else{
-        if(a > b) return a-b;
-        else if(a < b) return b-a;
-        else return 0;
+    if(a[0] !== b[0]){ // x가 같지 않으면 
+        return a[0] - b[0];
+    }else{ //x가 같으면
+        return a[1]-b[1];
     }
 }
 
 array.sort(compare);
 
-console.log(compare)
+let answer = '';
+for(item of array){
+    answer += `${item[0]} ${item[1]} \n`;
+}
+console.log(answer);
